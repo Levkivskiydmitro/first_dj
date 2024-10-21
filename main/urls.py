12c62957cf35fc5b92ff057f3b1867e211becda4
Dima <dimasmatko3@gmail.com>
2024-10-21 20:07:55 +0300

    Create feedback

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,13 @@
 from .settings import project
-from home import render_home, home
-from tour import render_tour, tour
+import home
+#from tour import render_tour, tour
 
 
 
-project.add_url_rule(rule='/', view_func=render_home)
+project.add_url_rule(rule='/', view_func= home.views.render_feedback)
 
-project.add_url_rule(rule='/tour', view_func=render_tour)
+#project.add_url_rule(rule='/tour', view_func=render_tour)
 
-project.register_blueprint(blueprint=home)
+project.register_blueprint(blueprint=home.home)
 
-project.register_blueprint(blueprint=tour)
+#project.register_blueprint(blueprint=tour)
